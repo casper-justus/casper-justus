af6d7c372f1855bf8d635ef3dfef9a0b21315edc
justus njagi <138237661+casper-justus@users.noreply.github.com>
2026-05-01 19:12:40 +0300

    Remove configuration settings from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/casper-justus",   // ← add this
-  images: {
-    unoptimized: true,
-  },
-};
+
 
 export default nextConfig;
